e9b31712ce83a6030e8962dcc8fad4501b05db32
aablan149 <anas.ablan@auto1.com>
2021-05-06 16:34:17 +0200

    Create git repository

---
+++ src/main/java/com/axiom/hyketask/HykeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.axiom.hyketask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HykeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HykeTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/axiom/hyketask/HykeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axiom.hyketask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HykeTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
