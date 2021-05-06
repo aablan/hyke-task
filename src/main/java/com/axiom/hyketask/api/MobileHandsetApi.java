f0c8b75827c5fe311951d46b9e7d2b21fc6fc08c
aablan149 <anas.ablan@auto1.com>
2021-05-06 17:23:49 +0200

    - fix endpoint url

--- src/main/java/com/axiom/hyketask/api/MobileHandsetApi.java
+++ src/main/java/com/axiom/hyketask/api/MobileHandsetApi.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient("mobile-handset")
+@FeignClient("mobile")
 public interface MobileHandsetApi {
 
     @GetMapping(
-            value = "/mobile-handset/search",
+            value = "/mobile/search",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     List<MobileHandsetDTO> mobileHandsets();
