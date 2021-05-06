a2cc129b7b62b2ccd55f6b1d0f125e8e228dfa3f
aablan149 <anas.ablan@auto1.com>
2021-05-06 17:19:38 +0200

    - api definition + dtos

---
+++ src/main/java/com/axiom/hyketask/dto/MobileHandsetDTO.java
@@ -0,0 +1,21 @@
+package com.axiom.hyketask.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MobileHandsetDTO {
+    private long id;
+    private String brand;
+    private String phone;
+    private String picture;
+    private ReleaseDTO release;
+    private String sim;
+    private String resolution;
+    private HardwareDTO hardware;
+}
