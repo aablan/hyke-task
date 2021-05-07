7def8d1a49432621a7d0217b05de994ca724e9ea
aablan149 <anas.ablan@auto1.com>
2021-05-07 13:45:16 +0200

    - Fix Dto types + add parameters object to api

--- src/main/java/com/axiom/hyketask/api/MobileHandsetApi.java
+++ src/main/java/com/axiom/hyketask/api/MobileHandsetApi.java
@@ -1,6 +1,8 @@
 package com.axiom.hyketask.api;
 
+import com.axiom.hyketask.dto.MobileFilterParamsDTO;
 import com.axiom.hyketask.dto.MobileHandsetDTO;
+import feign.QueryMap;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,5 +16,5 @@ public interface MobileHandsetApi {
             value = "/mobile/search",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    List<MobileHandsetDTO> mobileHandsets();
+    List<MobileHandsetDTO> mobileHandsets(@QueryMap MobileFilterParamsDTO params);
 }
